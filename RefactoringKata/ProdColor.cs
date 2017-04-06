d49b7301dce89bd92f1407573b3f3780627d9bab
Elton <st10426@hotmail.com>
2017-04-07 00:31:59 +0800

    add color and size dictionary

---
+++ RefactoringKata/ProdColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RefactoringKata
+{
+    public class ProdColor
+    {
+        public static string getColorName(int num)
+        {
+            Dictionary<int, string> colorMap = new Dictionary<int, string>()
+            {
+                {1, "blue"},
+                {2, "red"},
+                {3, "yellow"},
+            };
+
+            string color;
+            return colorMap.TryGetValue(num, out color) ? color : "no color";
+        }
+    }
+}
---
+++ RefactoringKata/ProdSize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace RefactoringKata
+{
+    public class ProdSize
+    {
+        public static string getSizeName(int num)
+        {
+            Dictionary<int, string> sizeMap = new Dictionary<int, string>()
+            {
+                {1, "XS"},
+                {2, "S"},
+                {3, "M"},
+                {4, "L"},
+                {5, "XL"},
+                {6, "XXL"}
+            };
+
+            string size;
+            return sizeMap.TryGetValue(num, out size) ? size : "Invalid Size";
+        }
+    }
+}
